22af8297aaef2ffbf3b64b88fe053c1bbc7d95a9
huqi <qing.hu@sinosun.com.cn>
2020-11-25 10:20:43 +0800

    增加文档；精简代码

--- springboot/src/main/java/com/sinosun/security/keycloack/controller/HelloController.java
+++ springboot/src/main/java/com/sinosun/security/keycloack/controller/HelloController.java
@@ -1,15 +1,12 @@
 package com.sinosun.security.keycloack.controller;
 
-import com.sinosun.security.keycloack.service.HelloService;
 import org.keycloak.KeycloakPrincipal;
-import org.keycloak.adapters.RefreshableKeycloakSecurityContext;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import java.security.Principal;
 
@@ -19,11 +16,15 @@ public class HelloController {
     @Autowired
     private HttpServletRequest request;
 
-    @Autowired
-    HelloService helloService;
-
+    /**
+     * 首页
+     * @param principal
+     * @param map
+     * @return
+     */
     @RequestMapping("/")
     public String indexPage(Principal principal, ModelMap map) {
+        System.out.println("/");
         map.addAttribute("host", "www.baidu.com");
         if (principal != null){
             map.addAttribute("user", principal.getName());
@@ -31,48 +32,50 @@ public class HelloController {
         return "index";
     }
 
+    /**
+     * 用户页面，登录才能访问
+     * @param principal
+     * @param map
+     * @return
+     */
     @RequestMapping("/customer")
     public String customer(Principal principal, ModelMap map) {
-
-        for (String key : map.keySet()){
-            System.out.println(key);
-        }
         System.out.println("/customer");
         map.addAttribute("role", "customer");
         map.addAttribute("name", principal.getName());
         return "customer";
     }
 
-    /**
-     * POST http://localhost:8080/auth/realms/<my_realm>/protocol/openid-connect/logout
-     Authorization: Bearer <access_token>
-     Content-Type: application/x-www-form-urlencoded
 
-     client_id=<my_client_id>&refresh_token=<refresh_token>
+    /**
+     * 登出
      * @param principal
      * @param map
      * @return
+     * @throws ServletException
      */
     @RequestMapping("/logout")
-    public String logout(Principal principal, ModelMap map) {
-
+    public String logout(Principal principal, ModelMap map) throws ServletException {
+        System.out.println("/logout");
         if (principal == null || !(principal instanceof KeycloakPrincipal)){
             return indexPage(principal,  map);
         }
-        KeycloakPrincipal keycloakPrincipal = (KeycloakPrincipal)principal;
-        RefreshableKeycloakSecurityContext securityContext = (RefreshableKeycloakSecurityContext)keycloakPrincipal.getKeycloakSecurityContext();
-//        String accessToken = securityContext.getTokenString();
-//        String refreshToken = securityContext.getRefreshToken();
-
-        boolean result = helloService.logout(securityContext);
-
-        map.addAttribute("result", result ? "logout success" : "logout failure");
-        return "logout";
+        request.logout();
+        return "redirect:/";
     }
 
+    /**
+     * 管理员页面
+     * @param map
+     * @return
+     */
     @RequestMapping("/admin")
     public String admin(ModelMap map) {
+        System.out.println("/admin");
         map.addAttribute("role", "admin");
         return "admin";
     }
+
+
+
 }
